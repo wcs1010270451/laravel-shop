ac7067ac42631718fe208a6d87b404760df8092d
wcs1010270451 <296952503@163.com>
2019-06-17 18:13:41 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://www.wcs21.top" target="_blank">wcs</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:296952503@163.com">联系我们</a></p>
+  </div>
+</footer>
