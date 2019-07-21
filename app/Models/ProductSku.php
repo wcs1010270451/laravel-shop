264e1eea4669664295c6c8f17b1af190095222eb
wcs1010270451 <296952503@163.com>
2019-07-21 18:22:07 +0800

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -14,4 +14,23 @@ class ProductSku extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    //减库存
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    //加库存
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
+    }
+
 }
