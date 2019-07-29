10ebdd7fd30c09884813f212a6de1e664d650f2e
wcs1010270451 <296952503@163.com>
2019-07-29 15:32:41 +0800

    用户界面-订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderRequest;
+use App\Http\Requests\Request;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -12,6 +13,18 @@ use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
+    //订单列表
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product','items.productSku'])
+            ->where('user_id',$request->user()->id)
+            ->orderBy('created_at','desc')
+            ->paginate();
+        return view('orders.index',['orders' => $orders]);
+    }
+
     //创建订单
     public function store(OrderRequest $request)
     {
